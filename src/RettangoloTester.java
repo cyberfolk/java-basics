c1a60440982e6689885336acc4b1ce831fe79468
cyberfolk <andrea1995ferrari@gmail.com>
2022-10-16 16:58:19 +0200

    Create Exercise N3 RettangoloTester.java

---
+++ src/RettangoloTester.java
@@ -0,0 +1,15 @@
+public class RettangoloTester {
+    public static void main(String[] args) {
+        Rettangolo rettangolo1 = new Rettangolo();
+        System.out.println("Perimetro: " + rettangolo1.getPerimetro());
+        System.out.println("Area: " + rettangolo1.getArea());
+        System.out.println("Expected perimetro: 4");
+        System.out.println("Expected area: 1");
+
+        Rettangolo rettangolo2 = new Rettangolo(5, 3, 9, 2);
+        System.out.println("Perimetro: " + rettangolo2.getPerimetro());
+        System.out.println("Area: " + rettangolo2.getArea());
+        System.out.println("Expected perimetro: 16");
+        System.out.println("Expected area: 15");
+    }
+}
