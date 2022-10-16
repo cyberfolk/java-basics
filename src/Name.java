04bea36285c9a9f0251fdaad6a522fe704a2cb47
cyberfolk <andrea1995ferrari@gmail.com>
2022-10-16 12:10:48 +0200

    Add Name.java

---
+++ src/Name.java
@@ -0,0 +1,16 @@
+/*
+ * Testo:
+ * Scrivere un programma che visualizzi sullo schermo del terminale il vostro nome
+ * all’interno di un rettangolo, come nell’esempio seguente:
+ *    + - - +
+ *    | Dave |
+ *    + - - + 
+ */
+
+public class Name {
+    public static void main(String[] args) throws Exception {
+        System.out.println("+ - -  - +");
+        System.out.println("| Andrea |");
+        System.out.println("+ - -  - +");
+}
+}
