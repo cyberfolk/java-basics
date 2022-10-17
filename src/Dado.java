f27a543ccbc9bb554ce11ee60869d1a9b0b9150b
cyberfolk <andrea1995ferrari@gmail.com>
2022-10-17 09:40:18 +0200

    Add Es4 DadoTester.java

---
+++ src/Dado.java
@@ -0,0 +1,28 @@
+/* ESERCIZIO 4
+ * Scrivere un programma che usi la classe Random per simulare il lancio di un
+ * dado, visualizzando un numero casuale compreso tra 1 e 6 ogni volta che viene
+ * eseguito.
+ */
+import java.util.Random;
+
+public class Dado {
+    private int nFacce;
+    private Random gen;
+
+    // costruisce un Dado a 6 facce
+    public Dado(int nFacce) {
+        this.nFacce = nFacce;
+        // si inizializza l'oggetto gen della classe Random
+        gen = new Random();
+    }
+
+    /**
+     * simula il lancio del dado
+     * 
+     * @return il risultato del lancio
+     */
+    public int lancia() {
+        int result = gen.nextInt(nFacce) + 1;
+        return result;
+    }
+}
---
+++ src/DadoTester.java
@@ -0,0 +1,14 @@
+/* ESERCIZIO 4
+ * Scrivere un programma che usi la classe Random per simulare il lancio di un
+ * dado, visualizzando un numero casuale compreso tra 1 e 6 ogni volta che viene
+ * eseguito.
+ */
+
+public class DadoTester {
+    public static void main(String[] args) {
+        Dado d10 = new Dado(10);
+        int risultato = d10.lancia();
+
+        System.out.println("Lancio: " + risultato);
+    }
+}
