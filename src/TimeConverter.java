05186d965628f451745ccacb765fbd2ad69376a0
cyberfolk <andrea1995ferrari@gmail.com>
2022-10-17 16:45:58 +0200

    Add SecondConverter and TimeConverter

---
+++ src/TimeConverter.java
@@ -0,0 +1,37 @@
+/* Es 4.2 – Tipi dati
+ * Scrivere un programma che, dato un certo tempo in giorni, ore, minuti e secondi,
+ * restituisca il numero totale di secondi. 
+ */
+
+public class TimeConverter {
+    private int secondi, minuti, ore, giorni;
+
+    /**
+     * si costruisce un covertitore da giorni, ore, minuti, secondi, in
+     * secondi totali
+     * 
+     * @param giorni giorni
+     * 
+     * @param ore ore
+     * 
+     * @param minuti minuti
+     * 
+     * @param secondi secondi
+     */
+    public TimeConverter(int giorni, int ore, int minuti, int secondi) {
+        this.giorni = giorni;
+        this.ore = ore;
+        this.minuti = minuti;
+        this.secondi = secondi;
+    }
+
+    /**
+     * si restituisce i secondi totali a partire da gg, h, min, sec inseriti
+     * 
+     * @return SecTot secondi totali
+     */
+    public int getSecTot() {
+        int sTot = giorni * 86400 + ore * 3600 + minuti * 60 + secondi;
+        return sTot;
+    }
+}
---
+++ src/TimeConverterTester.java
@@ -0,0 +1,23 @@
+/* Es 4.2 – Tipi dati
+ * Scrivere un programma che, dato un certo tempo in giorni, ore, minuti e secondi,
+ * restituisca il numero totale di secondi. 
+ */
+import java.util.Scanner;
+
+public class TimeConverterTester {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Inserisci il numero dei giorni ");
+        int dd = in.nextInt();
+        System.out.print("\nInserisci il numero delle ore ");
+        int hours = in.nextInt();
+        System.out.print("\nInserisci il numero dei minuti ");
+        int min = in.nextInt();
+        System.out.print("\nInserisci il numero dei secondi ");
+        int sec = in.nextInt();
+        TimeConverter conv = new TimeConverter(dd, hours, min, sec);
+        System.out.println("\nSecondi totali : " + conv.getSecTot());
+
+        in.close();
+    }
+}
