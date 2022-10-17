768b83f8fd9d1abfb7ae60d32f6a5bb9ff305bac
cyberfolk <andrea1995ferrari@gmail.com>
2022-10-17 10:38:33 +0200

    Add Es5 ContoTester.java

---
+++ src/Conto.java
@@ -0,0 +1,43 @@
+/* ESERCIZIO 5
+ * Scrivere un programma che costruisca un conto bancario, versi in esso
+ * $1000, prelevi da esso $500, prelevi altri $400 e infine visualizzi il saldo
+ * rimanente 
+ */
+public class Conto {
+    private double saldo;
+
+    /** Costruisce un conto con un saldo iniziale nullo    
+     *  
+     */
+    public Conto() {
+    }
+
+    /** Costruisce un conto con un saldo iniziale assegnato
+     * @param saldo Saldo iniziale Assegnato
+     */
+    public Conto(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /** Visualizza denaro sil conto
+     * @return  Il saldo del conto   
+     */
+    public double vediSaldo() {
+        return saldo;
+    }
+
+    /** Preleva denaro dal conto 
+     * @param prelievo Il prelievo da fare sul conto
+     */
+    public void preleva(int prelievo) {
+        this.saldo = this.saldo - prelievo;
+    }
+
+    /** Versa denaro nel conto 
+     * @param versamento Il versamento da fare sul conto
+     */
+    public void versa(int versamento) {
+        this.saldo = this.saldo + versamento;
+    }
+    
+}
---
+++ src/ContoTester.java
@@ -0,0 +1,17 @@
+/* ESERCIZIO 5
+ * Scrivere un programma che costruisca un conto bancario, versi in esso
+ * $1000, prelevi da esso $500, prelevi altri $400 e infine visualizzi il saldo
+ * rimanente 
+ */
+public class ContoTester {
+    public static void main(String[] args) {
+        Conto conto1 = new Conto();
+        conto1.versa(1000);
+        System.out.println("Sul conto ci sono "+conto1.vediSaldo()+"Euro");
+        conto1.preleva(500);;
+        System.out.println("Sul conto ci sono "+conto1.vediSaldo()+"Euro");
+        conto1.preleva(400);;
+        System.out.println("Sul conto ci sono "+conto1.vediSaldo()+"Euro");
+    }
+
+}
