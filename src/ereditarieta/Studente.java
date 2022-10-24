1dc9a4469627ae2d0f157d70b0fcce02fe5e08fe
cyberfolk <andrea1995ferrari@gmail.com>
2022-10-24 18:37:29 +0200

    Add Ereditarieta V01

---
+++ src/ereditarieta/Studente.java
@@ -0,0 +1,42 @@
+package ereditarieta;
+
+public class Studente extends Persona{
+    private String matricola;
+    private String universita;
+
+    public String toString(){
+        StringBuilder id = new StringBuilder();
+        id.append("Studente> ");
+        id.append("Nome: "+super.getNome()+". ");
+        id.append("Cognome: "+super.getCognome()+". ");
+        id.append("Codice Fiscale: "+super.getCodiceFiscale()+". ");
+        id.append("Matricola: "+matricola+". ");
+        id.append("Università: "+universita+".");
+        return id.toString();
+    } 
+
+    public Studente(String nome, String cognome, String codiceFiscale, String matricola, String universita) {
+        super(nome, cognome, codiceFiscale);
+        this.matricola = matricola;
+        this.universita = universita;
+    }
+
+    public Studente() {
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getUniversita() {
+        return universita;
+    }
+    public void setUniversita(String universita) {
+        this.universita = universita;
+    }
+
+    
+}
